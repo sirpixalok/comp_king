0f340047b170f2055176c27f4d3c99784b218588
sirpixalok <36656440+sirpixalok@users.noreply.github.com>
2025-08-11 16:14:14 -0400

    Add Card data class

---
+++ app/src/main/java/com/compking/app/Card.kt
@@ -0,0 +1,9 @@
+package com.compking.app
+
+data class Card(
+    val playerName: String = "",
+    val cardSet: String = "",
+    val year: Int = 0,
+    val salePrice: Double = 0.0,
+    val saleDate: String = ""
+)
